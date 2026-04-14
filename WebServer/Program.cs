41d4e11e5a52d600710e90dc846654ed6060a844
vika <vikatekutova7@gmail.com>
2026-04-14 14:42:25 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,6 +3,18 @@ using System.Data;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) => {
+    Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
+    await next(context);
+    Console.WriteLine($"[LOG] Ответ отправлена {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) => {
+    context.Response.Headers.Append("X-Powered-By", "ASP.NET Core Lab27");
+    await next(context);
+});
+
+
 app.MapGet("/", () => "Добро пожаловать на сервер!");
 
 app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
